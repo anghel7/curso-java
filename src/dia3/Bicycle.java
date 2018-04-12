f859a8bf60efd6efe1467861d6ff3ba4901a8698
Angel Antezana Flores <ghehahanghel@gmail.com>
2018-04-12 19:27:07 -0400

    Instanciar student a bae de la person

--- src/dia3/Bicycle.java
+++ src/dia3/Bicycle.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package dia3;
+import dia2.Person;
 
 /**
  *
@@ -15,10 +16,17 @@ public class Bicycle {
     private int gear = 1;
     private double price;
     private String brand;
+    private Person owner;
 
     public Bicycle(double price, String brand) {
         this.price = price;
         this.brand = brand;
+    }
+    
+    public Bicycle(double price, String brand, Person owner) {
+        this.price = price;
+        this.brand = brand;
+        this.owner =owner;
     }
     
      public Bicycle(double price, String brand, int gear) {
@@ -43,9 +51,17 @@ public class Bicycle {
         this.speed = this.speed +1;
     }   
     
-    public void incrementSpeed(int speed){
-        this.speed = this.speed + speed;
+    public void incrementSpeed(int increment){
+        this.speed = this.speed + increment;
     }    
+    
+    public void applyBrakes(int decrement){
+        if(decrement <= this.speed){
+            this.speed = this.speed - decrement;
+        }else{
+            this.speed = 0;
+        }        
+    }
     
     public double getPrice() {
         return price;
--- src/dia3/Main.java
+++ src/dia3/Main.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package dia3;
-
+import dia2.Person;
+import dia2.Student;
+import dia2.Teacher;
 /**
  *
  * @author skypper
@@ -34,7 +36,21 @@ public class Main {
         bicycle3.incrementSpeed();
         bicycle3.incrementSpeed(7);
         bicycle3.incrementSpeed(9);
+        bicycle3.applyBrakes(17);
         System.out.println(bicycle3.getStates());
+        
+        System.out.println("iNTERACTUANDO CON OTROS OBJETOS");
+        
+        Person person4;
+        
+        Student student4;
+        student4 = new Student("Jorge", "Meneces", "jun 23, 1994", "5647456", "2010978");
+        
+        person4 = new Teacher("Marco", "Nogales", "jun 11, 1992", "666", "Ing sistem");
+        
+        Bicycle bicycle4;
+        bicycle4 = new Bicycle(6000, "Dell", person4);
+        
         
     }
 }
